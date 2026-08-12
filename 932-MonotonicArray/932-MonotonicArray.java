2128f92e7bd2ea938dd4dfce7b5c6d26221ba1c5
DharshiniA9606 <dharshinianandhan6@gmail.com>
2026-08-12 08:43:47 +0530

    Create 932-MonotonicArray

---
+++ 932-MonotonicArray/932-MonotonicArray.java
@@ -0,0 +1,21 @@
+// Last updated: 8/12/2026, 8:43:48 AM
+class Solution {
+    public boolean isMonotonic(int[] nums) {
+        boolean increasing = true;
+        boolean decreasing = true;
+
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] > nums[i - 1]) {
+                decreasing = false;
+            } else if (nums[i] < nums[i - 1]) {
+                increasing = false;
+            }
+
+            if (!increasing && !decreasing) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
