7058e95c45613e64dc7e8f24b3be6e9d4665fc54
DharshiniA9606 <dharshinianandhan6@gmail.com>
2026-08-12 08:41:44 +0530

    Create 1823-DetermineIfStringHalvesAreAlike

---
+++ 1823-DetermineIfStringHalvesAreAlike/1823-DetermineIfStringHalvesAreAlike.java
@@ -0,0 +1,28 @@
+// Last updated: 8/12/2026, 8:41:45 AM
+class Solution {
+    public boolean halvesAreAlike(String s) {
+        int count1 = 0;
+        int count2 = 0;
+
+        int mid = s.length() / 2;
+
+        for (int i = 0; i < mid; i++) {
+            if (isVowel(s.charAt(i))) {
+                count1++;
+            }
+        }
+
+        for (int i = mid; i < s.length(); i++) {
+            if (isVowel(s.charAt(i))) {
+                count2++;
+            }
+        }
+
+        return count1 == count2;
+    }
+
+    public boolean isVowel(char ch) {
+        return ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' ||
+               ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U';
+    }
+}
