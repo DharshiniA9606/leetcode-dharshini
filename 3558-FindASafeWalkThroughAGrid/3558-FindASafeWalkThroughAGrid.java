de10bedaa15840947179ec2cacba1be614bb34cf
DharshiniA9606 <dharshinianandhan6@gmail.com>
2026-08-12 08:40:08 +0530

    Create 3558-FindASafeWalkThroughAGrid

---
+++ 3558-FindASafeWalkThroughAGrid/3558-FindASafeWalkThroughAGrid.java
@@ -0,0 +1,65 @@
+// Last updated: 8/12/2026, 8:40:09 AM
+class Solution {
+    public boolean findSafeWalk(List<List<Integer>> grid, int health) {
+
+        int m = grid.size();
+        int n = grid.get(0).size();
+
+        int[][] dist = new int[m][n];
+
+        for (int i = 0; i < m; i++)
+            Arrays.fill(dist[i], Integer.MAX_VALUE);
+
+        PriorityQueue<int[]> pq =
+                new PriorityQueue<>((a, b) -> a[0] - b[0]);
+
+        dist[0][0] = grid.get(0).get(0);
+
+        pq.offer(new int[]{
+                dist[0][0],
+                0,
+                0
+        });
+
+        int[] dr = {-1, 1, 0, 0};
+        int[] dc = {0, 0, -1, 1};
+
+        while (!pq.isEmpty()) {
+
+            int[] cur = pq.poll();
+
+            int cost = cur[0];
+            int r = cur[1];
+            int c = cur[2];
+
+            if (cost > dist[r][c])
+                continue;
+
+            for (int k = 0; k < 4; k++) {
+
+                int nr = r + dr[k];
+                int nc = c + dc[k];
+
+                if (nr >= 0 && nr < m &&
+                    nc >= 0 && nc < n) {
+
+                    int newCost =
+                        cost + grid.get(nr).get(nc);
+
+                    if (newCost < dist[nr][nc]) {
+
+                        dist[nr][nc] = newCost;
+
+                        pq.offer(new int[]{
+                            newCost,
+                            nr,
+                            nc
+                        });
+                    }
+                }
+            }
+        }
+
+        return dist[m - 1][n - 1] < health;
+    }
+}
