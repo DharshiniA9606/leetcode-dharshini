46246ec3e807c139f924832b87ce6a1158e1705d
DharshiniA9606 <dharshinianandhan6@gmail.com>
2026-08-12 08:44:58 +0530

    Create 645-SetMismatch

---
+++ 645-SetMismatch/645-SetMismatch.java
@@ -0,0 +1,28 @@
+// Last updated: 8/12/2026, 8:44:59 AM
+class Solution {
+    public int[] findErrorNums(int[] nums) {
+
+        HashSet<Integer> set = new HashSet<>();
+
+        int duplicate = -1;
+
+        for (int num : nums) {
+            if (set.contains(num)) {
+                duplicate = num;
+            } else {
+                set.add(num);
+            }
+        }
+
+        int missing = -1;
+
+        for (int i = 1; i <= nums.length; i++) {
+            if (!set.contains(i)) {
+                missing = i;
+                break;
+            }
+        }
+
+        return new int[]{duplicate, missing};
+    }
+}
