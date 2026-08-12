c9a03d7f70216c19f22e5d7637cd88333b3b7729
DharshiniA9606 <dharshinianandhan6@gmail.com>
2026-08-12 08:43:28 +0530

    Create 974-ReorderDataInLogFiles

---
+++ 974-ReorderDataInLogFiles/974-ReorderDataInLogFiles.java
@@ -0,0 +1,19 @@
+// Last updated: 8/12/2026, 8:43:29 AM
+class Solution {
+    public String[] reorderLogFiles(String[] logs) {
+        Arrays.sort(logs, (a, b)->{
+            int i = a.indexOf(" ");
+            int j = b.indexOf(" ");
+            if(Character.isDigit(a.charAt(i+1)) && Character.isDigit(b.charAt(j+1))) return 0;
+            if(Character.isDigit(a.charAt(i+1))) return 1;
+            if(Character.isDigit(b.charAt(j+1))) return -1;
+            int val = a.substring(i+1).compareTo(b.substring(j+1));
+            if(val != 0){
+                return val;
+            }
+            return a.compareTo(b);
+        });
+
+        return logs;
+    }
+}
