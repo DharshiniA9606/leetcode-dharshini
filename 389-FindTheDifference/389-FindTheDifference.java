ed096f6aad7ec2937b07b301749df0ec03c8b061
DharshiniA9606 <dharshinianandhan6@gmail.com>
2026-08-12 08:46:35 +0530

    Create 389-FindTheDifference

---
+++ 389-FindTheDifference/389-FindTheDifference.java
@@ -0,0 +1,16 @@
+// Last updated: 8/12/2026, 8:46:36 AM
+class Solution {
+    public char findTheDifference(String s, String t) {
+        long sum = 0, diff = 0;
+
+        for (char c : t.toCharArray()) {
+            sum += c - 'a';
+        }
+
+        for (char c : s.toCharArray()) {
+            diff += c - 'a';
+        }
+
+        return (char) (sum - diff + 'a');
+    }
+}
