1ebbb0653bdfb26bcd8b2aa87dca3955a6ccabed
DharshiniA9606 <dharshinianandhan6@gmail.com>
2026-08-12 08:44:08 +0530

    Create 812-RotateString

---
+++ 812-RotateString/812-RotateString.java
@@ -0,0 +1,29 @@
+// Last updated: 8/12/2026, 8:44:09 AM
+class Solution {
+    public boolean rotateString(String A, String B) {
+        if(A == null || B == null) {
+            return false;
+        }
+        if(A.length() != B.length()) {
+            return false;
+        }
+        if(A.length() == 0) {
+            return true;
+        }
+        for(int i = 0; i < A.length(); i++) {
+            if(rotateString(A, B, i)) {
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    private boolean rotateString(String A, String B, int rotation) {
+        for(int i = 0; i < A.length(); i++) {
+            if(A.charAt(i) != B.charAt((i+rotation)%B.length())) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
