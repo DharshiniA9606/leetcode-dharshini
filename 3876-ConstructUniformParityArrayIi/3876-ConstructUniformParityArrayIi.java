3f4abe12418b96825c9d76432a03c3206d87e18b
DharshiniA9606 <dharshinianandhan6@gmail.com>
2026-09-12 16:28:19 +0530

    Create 3876-ConstructUniformParityArrayIi

---
+++ 3876-ConstructUniformParityArrayIi/3876-ConstructUniformParityArrayIi.java
@@ -0,0 +1,14 @@
+// Last updated: 9/12/2026, 4:28:19 PM
+class Solution {
+    public boolean uniformArray(int[] nums1) {
+        int mn = Integer.MAX_VALUE;
+        int count = 0;
+
+        for (int x : nums1) {
+            mn = Math.min(mn, x);
+            if (x % 2 == 1) count++;
+        }
+
+        return mn % 2 == 1 || count == 0;
+    }
+}
