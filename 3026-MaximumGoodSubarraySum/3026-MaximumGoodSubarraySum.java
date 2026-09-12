1e60f3d6fcee4aa16a3587d7f89cf7fa618bb7a4
DharshiniA9606 <dharshinianandhan6@gmail.com>
2026-09-12 16:29:17 +0530

    Create 3026-MaximumGoodSubarraySum

---
+++ 3026-MaximumGoodSubarraySum/3026-MaximumGoodSubarraySum.java
@@ -0,0 +1,26 @@
+// Last updated: 9/12/2026, 4:29:17 PM
+class Solution {
+    public long maximumSubarraySum(int[] nums, int k) {
+        int n = nums.length;
+        Map<Integer,Long> map = new HashMap<>();
+        long prefixsum=0;
+        long maxSum=Long.MIN_VALUE;
+        for(int i=0;i<n;i++)
+        {
+            if(map.getOrDefault(nums[i],Long.MAX_VALUE)>prefixsum)
+            {
+                 map.put(nums[i],prefixsum);
+            }
+            prefixsum+=nums[i];
+            if(map.containsKey(nums[i]-k))
+            {
+                 maxSum=Math.max(maxSum,prefixsum-map.get(nums[i]-k));
+            }
+            if(map.containsKey(nums[i]+k))
+            {
+                 maxSum=Math.max(maxSum,prefixsum-map.get(nums[i]+k));
+            }
+        }
+        return maxSum==Long.MIN_VALUE ? 0 : maxSum;
+    }
+}
